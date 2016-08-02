0308ede38634a5b5f77e2c442c603f186b25add7
jingwang88 <wj154419@163.com>
2016-08-02 21:14:01 +0800

    start js

--- main.js
+++ main.js
@@ -1,11 +1,92 @@
-var configs = [
-    {
-        type: "科技",
-        title: "科技改变世界改变生活改变自己",
-        description: "",
-        img: ""
-    }
-];
-function createDisplayBOx (config) {
-
+function innsertDirectoryData (configs, videoList) {
+    var html = '';
+    configs.forEach(function (config, index) {
+        html = '';
+        html += '<li class="list-item clearfix"><div class="description-img"><img src="' + config.img + '" class="descript-img"></div><div class="description-info"><a href="' + config.url + '" target="_blank"><span class="info-name">' + config.title + '</span></a><a href="' + config.url + '" target="_blank"><span class="info-link">' + config.url + '</span></a></div></li>';
+        $(videoList).append(html);
+    });
 }
+
+function  loadVideo(config, column) {
+    //每次刷新的class 都不同
+    var listChunkClass = 'list'+(Math.floor((config.rank-1)/5) + 1);
+    var html = '<div class="video-content ' + listChunkClass + '"><div class="video-info-container"><span class="video-info-type">' + config.type +'</span><h2 class="video-info-title"><a href="' + config.url + '">' + config.title + '</a></h2><p class="video-info-description">' + config.description +'</p></div>'+
+                '<div class="video-img-container"><a href="' + config.url +'"><div class="video-img"><img src="' + config.pictrue + '" class="cover-img" /><img src="./img/play.png" class="play-btn" /></div><div class="cover-shadow"></div></a></div></div>';
+    $(column).append(html);
+}
+
+
+$(function () {
+    // 一次性载入多个展示盒子
+    function refreshVideoList (videoConfigList) {
+        var listChunkClass = 'list'+(Math.floor((videoConfigList[0].rank-1)/5) + 1);
+        if (windowWidth > 1157) {
+            videoConfigList.forEach(function (config) {
+                loadVideo(config, $('.video-column')[startColumnNum]);
+                startColumnNum = startColumnNum >= 2 ? 0 : (startColumnNum+1);
+            });
+        } else if (windowWidth > 615) {
+            videoConfigList.forEach(function (config) {
+                loadVideo(config, $('.video-column')[startColumnNum]);
+                startColumnNum = startColumnNum >= 1 ? 0 : (startColumnNum+1);
+            })
+        } else {
+            videoConfigList.forEach(function (config) {
+                loadVideo(config, $('.video-column')[0]);
+            })
+        }
+        $('.video-content.'+listChunkClass).hide();
+        $('.video-content.'+listChunkClass).fadeIn(3000);
+    }
+
+    // 初始化页面数据
+    $.each($('.video-list'), function (index, ele) {
+        console.log(ele);
+        innsertDirectoryData(directoryConfigs[index], ele);
+    });
+
+    var windowWidth =$('body').width();
+    var startColumnNum = 0;
+    var refreshNum = 1;
+    refreshVideoList (videoConfigList);
+    $(document).bind('scroll', function (event) {
+        var clientHeight = document.documentElement.scrollHeight;
+        var scrollHeight = document.documentElement.clientHeight + window.scrollY;
+        if (clientHeight === scrollHeight) {
+            $('.load-notice').css('display', 'block');
+            $.ajax({
+                type: "GET",
+                url: "www.baidu.com",
+                data: {refreshNum: ++refreshNum},
+                dataType: "json",
+                beforeSend: function (xhr) {
+                    console.log("before send");
+                },
+                success: function (data, textStauts) {
+                    console.log("success");
+                    refreshVideoList(data);
+                    $('.load-notice').css('display', 'none');
+                },
+                error: function (xhr, textStauts, error) {
+/*                    throw throwError;
+*/                  console.log("error");
+                    refreshNum--;
+                    $('.load-notice').css('display', 'none');
+                },
+                complete: function () {
+                    console.log('complete');
+                }
+            });
+        }
+    });
+
+    
+
+
+    // 动画
+    $('.video-img-container').hover(function() {
+        $(this).children('a').children('.cover-shadow').show();
+    }, function () {
+        $(this).children('a').children('.cover-shadow').hide();
+    });
+});
---
+++ src/data.js
@@ -0,0 +1,325 @@
+var techDireConfigs = [
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    }
+];
+
+var zongyiDireConfigs = [
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    }
+];
+var militaryDireConfigs = [
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    }
+];
+var tvDireConfigs = [
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    }
+];
+
+var movieDireConfigs = [
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    }
+];
+
+var otherDireConfigs = [
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    },
+    {
+        type: "科技",
+        title: "科技改变世界改变生活改变自己",
+        description: "",
+        img: "./img/descript.jpeg",
+        url: 'http://www.mgtv.com/'
+    }
+];
+
+var directoryConfigs = [techDireConfigs, zongyiDireConfigs, militaryDireConfigs, tvDireConfigs, movieDireConfigs, otherDireConfigs];
+
+var videoConfigList = [
+        {
+            type: "科技",
+            title: "眼下的夏天，是地地道道的夏天。太阳才一露脸。这是一个长满了百合花的峡谷。百合花静静地开放着",
+            description: "这是一个长满了百合花的峡谷。百合花静静地开放着，水边、坡上、岩石旁、大树下，到处都有。它们不疯不闹，也无鲜艳的颜色。",
+            img: "./img/cover.jpg",
+            url: 'http://www.mgtv.com/',
+            pictrue: "./img/cover.jpg",
+            rank: 1
+        },
+        {
+            type: "科技",
+            title: "眼下的夏天，是地地道道的夏天。太阳才一露脸。这是一个长满了百合花的峡谷。百合花静静地开放着",
+            description: "这是一个长满了百合花的峡谷。这是一个长满了百合花的峡谷。这是一个长满了百合花的峡谷。百合花静静地开放着，水边、坡上、岩石旁、大树下，到处都有。它们不疯不闹，也无鲜艳的颜色。",
+            img: "./img/cover.jpg",
+            url: 'http://www.mgtv.com/',
+            pictrue: "./img/cover.jpg",
+            rank: 2
+        },
+        {
+            type: "科技",
+            title: "眼下的夏天，是地地道道的夏天。太阳才一露脸。这是一个长满了百合花的峡谷。这是一个长满了百合花的峡谷。这是一个长满了百合花的峡谷。百合花静静地开放着",
+            description: "这是一个长满了百合花的峡谷。百合花静静地开放着，水边、坡上、岩石旁、大树下，到处都有。它们不疯不闹，也无鲜艳的颜色。",
+            img: "./img/cover.jpg",
+            url: 'http://www.mgtv.com/',
+            pictrue: "./img/cover.jpg",
+            rank: 3
+        },
+        {
+            type: "科技",
+            title: "眼下的夏天，是地地道道的夏天。太阳才一露脸。这是一个长满了百合花的峡谷。百合花静静地开放着",
+            description: "这是一个长这是一个长满了百合花的峡谷。满了百合花的峡谷。百合花静静地开放着，这是一个长满了百合花的峡谷。水边、坡上、岩石旁、大树下，到处都有。它们不疯不闹，也无鲜艳的颜色。",
+            img: "./img/cover.jpg",
+            url: 'http://www.mgtv.com/',
+            pictrue: "./img/cover.jpg",
+            rank: 4
+        },
+        {
+            type: "科技",
+            title: "眼下的夏天，是地地道道的夏天。太阳才一露脸。这是一个长满了百合花的峡谷。百合花静静地开放着",
+            description: "这是一个长满了百合花的峡谷。这是一个长满了百合花的峡谷。这是一个长满了百合花的峡谷。百合花静静地开放着，水边、坡上、岩石旁、大树下，到处都有。它们不疯不闹，也无鲜艳的颜色。",
+            img: "./img/cover.jpg",
+            url: 'http://www.mgtv.com/',
+            pictrue: "./img/cover.jpg",
+            rank: 5
+        }
+];
+
+var videoConfigList1 = [
+        {
+            type: "科技",
+            title: "眼下的夏天，是地地道道的夏天。太阳才一露脸。这是一个长满了百合花的峡谷。百合花静静地开放着",
+            description: "这是一个长满了百合花的峡谷。百合花静静地开放着，水边、坡上、岩石旁、大树下，到处都有。它们不疯不闹，也无鲜艳的颜色。",
+            img: "./img/cover.jpg",
+            url: 'http://www.mgtv.com/',
+            pictrue: "./img/cover.jpg",
+            rank: 6
+        },
+        {
+            type: "科技",
+            title: "眼下的夏天，是地地道道的夏天。太阳才一露脸。这是一个长满了百合花的峡谷。百合花静静地开放着",
+            description: "这是一个长满了百合花的峡谷。这是一个长满了百合花的峡谷。这是一个长满了百合花的峡谷。百合花静静地开放着，水边、坡上、岩石旁、大树下，到处都有。它们不疯不闹，也无鲜艳的颜色。",
+            img: "./img/cover.jpg",
+            url: 'http://www.mgtv.com/',
+            pictrue: "./img/cover.jpg",
+            rank: 7
+        },
+        {
+            type: "科技",
+            title: "眼下的夏天，是地地道道的夏天。太阳才一露脸。这是一个长满了百合花的峡谷。这是一个长满了百合花的峡谷。这是一个长满了百合花的峡谷。百合花静静地开放着",
+            description: "这是一个长满了百合花的峡谷。百合花静静地开放着，水边、坡上、岩石旁、大树下，到处都有。它们不疯不闹，也无鲜艳的颜色。",
+            img: "./img/cover.jpg",
+            url: 'http://www.mgtv.com/',
+            pictrue: "./img/cover.jpg",
+            rank: 8
+        },
+        {
+            type: "科技",
+            title: "眼下的夏天，是地地道道的夏天。太阳才一露脸。这是一个长满了百合花的峡谷。百合花静静地开放着",
+            description: "这是一个长这是一个长满了百合花的峡谷。满了百合花的峡谷。百合花静静地开放着，这是一个长满了百合花的峡谷。水边、坡上、岩石旁、大树下，到处都有。它们不疯不闹，也无鲜艳的颜色。",
+            img: "./img/cover.jpg",
+            url: 'http://www.mgtv.com/',
+            pictrue: "./img/cover.jpg",
+            rank: 9
+        },
+        {
+            type: "科技",
+            title: "眼下的夏天，是地地道道的夏天。太阳才一露脸。这是一个长满了百合花的峡谷。百合花静静地开放着",
+            description: "这是一个长满了百合花的峡谷。这是一个长满了百合花的峡谷。这是一个长满了百合花的峡谷。百合花静静地开放着，水边、坡上、岩石旁、大树下，到处都有。它们不疯不闹，也无鲜艳的颜色。",
+            img: "./img/cover.jpg",
+            url: 'http://www.mgtv.com/',
+            pictrue: "./img/cover.jpg",
+            rank: 10
+        }
+];
+
+
